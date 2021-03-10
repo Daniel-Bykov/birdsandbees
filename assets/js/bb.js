c8c36fa74038b0499b00713be94540ba4585750b
Daniel Bykov <daniel-bykov@yandex.ru>
2021-03-10 20:58:40 +0400

    tabs slider

--- assets/js/bb.js
+++ assets/js/bb.js
@@ -6,18 +6,52 @@ document.addEventListener('DOMContentLoaded', function () {
 
   console.log(burger, lm, close);
 
-    burger.addEventListener('click', function () {
-        lm.style.visibility = "visible";
-        lm.style.display = "flex" });
+  burger.addEventListener('click', function () {
+    lm.style.visibility = "visible";
+    lm.style.display = "flex"
+  });
 
-        close.addEventListener('click', function () {
-          lm.style.visibility = "hidden"
-          lm.style.display = "none"   });
-  
-          
-     
-
+  close.addEventListener('click', function () {
+    lm.style.visibility = "hidden"
+    lm.style.display = "none"
+  });
 });
 
 
+let tab; 
+let tabContent; 
+
+
+window.onload=function() {
+    tabContent=document.getElementsByClassName('tabContent');
+    tab=document.getElementsByClassName('tab');
+    hideTabsContent(1);
+}
+
+document.getElementById('tabs').addEventListener('click', function (event)  {
+    let target=event.target;
+    if (target.className=='tab') {
+       for (let i=0; i<tab.length; i++) {
+           if (target == tab[i]) {
+               showTabsContent(i);
+               break;
+           }
+       }
+    }
+});
+
+function hideTabsContent(a) {
+    for (let i=a; i<tabContent.length; i++) {
+        tabContent[i].classList.remove('show');
+        tabContent[i].classList.add("hide");
+    }
+}
+
+function showTabsContent(b){
+    if (tabContent[b].classList.contains('hide')) {
+        hideTabsContent(0);
+        tabContent[b].classList.remove('hide');
+        tabContent[b].classList.add('show');
+    }
+}
 
