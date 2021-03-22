e308826962e8f51d203af86a52ac6e7da27c9820
Daniel Bykov <bykovdaniel@gmail.com>
2021-03-22 10:57:44 +0400

    final 0.2

--- assets/js/sidebar-mobile.js
+++ assets/js/sidebar-mobile.js
@@ -5,21 +5,27 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('#mobile-menu-active').addEventListener('click', () => {
         
         document.querySelector('.left-menu-mobile-box').classList.add('left-menu-mobile-box--active')
-        document.querySelector('.left-menu-mobile-box--active').style.transition = "1s linear";
+        document.querySelector('.mobile-menu-button').classList.add('mobile-menu-button--active')
         document.body.classList.add("body--veil")
-    });
 
-    document.querySelector('.close-menu').addEventListener('click', function () {
-        document.body.classList.remove("body--veil")
-        document.querySelector('.left-menu-mobile-box').classList.remove('left-menu-mobile-box--active');
-    });
 
-    document.querySelector('.left-menu-mobile-box').addEventListener('click', function () {
-        document.body.classList.remove("body--veil")
-        document.querySelector('.left-menu-mobile-box').classList.remove('left-menu-mobile-box--active');
-    });
+        let activeSideBar = document.querySelector('.mobile-menu-button--active');
+
+        if(activeSideBar){
+            console.log("hola!")
+            document.querySelector('.mobile-menu-button--active').addEventListener('click', function () {
+                
+            document.body.classList.remove("body--veil")
+            document.querySelector('.left-menu-mobile-box').classList.remove('left-menu-mobile-box--active');
+        });
+        }
+
+        
 
     document.querySelector('.left-menu-mobile-content').addEventListener('click', function (e) {
         e.stopPropagation();
     });
+    });
+
+
 });
